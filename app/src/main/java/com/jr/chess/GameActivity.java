e27f29ce4bd1611b46d68f2b14637c0226eace93
jakubriegel <jakubriegel@hotmail.com>
2017-11-23 22:57:41 +0100

    GameActivity added and styled to full screen

--- app/src/main/java/com/jr/chess/GameActivity.java
+++ app/src/main/java/com/jr/chess/GameActivity.java
@@ -1,7 +1,9 @@
 package com.jr.chess;
 
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class GameActivity extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        this.goFullscreen();
+
+    }
+
+    private void goFullscreen(){
+        ActionBar actionBar = getActionBar();
+        if (actionBar != null) actionBar.hide();
+
+        int newUiOptions = getWindow().getDecorView().getSystemUiVisibility();
+        newUiOptions ^= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        newUiOptions ^= View.SYSTEM_UI_FLAG_FULLSCREEN;
+        newUiOptions ^= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        getWindow().getDecorView().setSystemUiVisibility(newUiOptions);
     }
 }
--- app/src/main/java/com/jr/chess/MainActivity.java
+++ app/src/main/java/com/jr/chess/MainActivity.java
@@ -1,7 +1,11 @@
 package com.jr.chess;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent. ACTION_PICK_ACTIVITY);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, "test");
+        sendIntent.setType("text/plain");
+
+
+        Button startGameButton = findViewById(R.id.start_game_button);
+        startGameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startGameActivity = new Intent(getApplicationContext(), GameActivity.class);
+                startActivity(startGameActivity);
+            }
+        });
+
+
+
+
     }
+
 }
