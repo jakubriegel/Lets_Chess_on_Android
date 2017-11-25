9dce61fe8371e86ab05ceadf51eb2cc8eefc48e3
jakubriegel <jakubriegel@hotmail.com>
2017-11-25 22:31:57 +0100

    chessboard added, drawing in progress
    chessboard styling(size and background) - done
    
    canvas:
    experimenting with drawing logic

--- app/src/main/java/com/jr/chess/GameActivity.java
+++ app/src/main/java/com/jr/chess/GameActivity.java
@@ -1,23 +1,36 @@
 package com.jr.chess;
 
+import android.annotation.SuppressLint;
 import android.app.ActionBar;
+import android.content.Context;
+import android.content.res.TypedArray;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.Window;
 
-public class GameActivity extends AppCompatActivity {
+import com.jr.chess.views.Board;
+
+public class GameActivity extends AppCompatActivity{
+
+    Board board;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-
         this.goFullscreen();
+
+        board = findViewById(R.id.board_view);
+        board.invalidate();
 
     }
 
     private void goFullscreen(){
-        ActionBar actionBar = getActionBar();
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) actionBar.hide();
 
         int newUiOptions = getWindow().getDecorView().getSystemUiVisibility();
@@ -26,4 +39,18 @@ public class GameActivity extends AppCompatActivity {
         newUiOptions ^= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
         getWindow().getDecorView().setSystemUiVisibility(newUiOptions);
     }
+
+
+
+    /*public class Board extends View{
+
+        public Board(Context context, AttributeSet attrs) {
+            super(context, attrs);
+
+        }
+
+        /*@SuppressLint("ClickableViewAccessibility")
+
+    }*/
+
 }
---
+++ app/src/main/java/com/jr/chess/views/Board.java
@@ -0,0 +1,68 @@
+package com.jr.chess.views;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewParent;
+
+
+public class Board extends View {
+    public Board(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        View parent = (View) this.getParent();
+        setMeasuredDimension((int) (parent.getWidth() * .95), (int) (parent.getWidth() * .95));
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                /*String txt = "x: " + (int)event.getX() + " y: " + (int)event.getY();
+                Log.v("motion_test", txt);*/
+                getSquare((int)event.getX(), (int)event.getY());
+        }
+
+        return super.onTouchEvent(event);
+    }
+
+    private void getSquare(int x, int y){
+        int oneWidth = this.getWidth() / 8;
+        String txt = "x: " + x/oneWidth + " y: " + (7-(y/oneWidth));
+        Log.v("motion_test", txt);
+    }
+
+    private class Square{
+        public Square(){
+
+        }
+    }
+
+    private boolean con = false;
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Paint p = new Paint();
+        p.setColor(Color.RED);
+        p.setStyle(Paint.Style.FILL);
+
+        if(con) canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
+        con = true;
+        canvas.drawCircle(250, 250, 50, p);
+    }
+}
